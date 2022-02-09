92b1365b097e5150d6844a38316b42e9939c0f6a
Sergei Solodovnikov <sergeisolodovnikov0@gmail.com>
2022-02-10 00:55:50 +0300

    Initial commit

---
+++ src/Praktikum.java
@@ -0,0 +1,22 @@
+public class Praktikum {
+    public static void main(String[] args) {
+
+        double rubles = 14560.5;
+        double rateUSD = 78.5;
+        double rateEUR = 85.5;
+        double rateJPY = 0.74; // Курс японской иены
+
+        String currency = "JPY";
+        System.out.println("Вы конвертируете рубли в " + currency);
+
+        if (currency.equals("USD")) {
+            System.out.println("Ваши сбережения в долларах: " + rubles / rateUSD);
+        } else if (currency.equals("EUR")) {
+            System.out.println("Ваши сбережения в евро: " + rubles / rateEUR);
+        } else if (currency.equals("JPY")) {
+            System.out.println("Ваши сбережения в иенах: " + rubles / rateJPY);
+        } else if (currency.equals("DKK")) {
+            System.out.println("Валюта не поддерживается.");
+        }
+    }
+}
